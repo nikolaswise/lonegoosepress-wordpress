e89568958dde5689a654786e283bd80f5c07f34d
Nathan Searles <nsearles@gmail.com>
2016-05-06 16:05:16 -0700

    added foundation grid and base markup to header

--- wp-content/themes/fuzzco/header.php
+++ wp-content/themes/fuzzco/header.php
@@ -8,36 +8,53 @@
  *
  * @package fuzzco
  */
-
-?><!DOCTYPE html>
+?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<!-- Typekit -->
+<script>
+	(function(d) {
+		var config = {
+			kitId: 'kxw4hri',
+			scriptTimeout: 3000,
+			async: true
+		},
+		h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+	})(document);
+</script>
 
 <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class(); ?> id="page-<?php echo is_front_page() ? 'home' : get_query_var('name');?>">
 <div id="page" class="site">
 
 	<header class="site-header" role="banner">
+		  <div class="row">
+        <div class="xlarge-4 columns">
+          <p>
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="logo"><?php bloginfo( 'name' ); ?></a>
+          </p>
+        </div>
+        <div class="xlarge-8 columns">
+          <nav id="site-navigation" class="main-navigation" role="navigation">
+            <?php
+              $args = array(
+                'container' => 'ul',
+                'menu_class' => 'menu',
+                'menu_id' => ''
+              );
 
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif; ?>
-
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'fuzzco' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+              wp_nav_menu( $args );
+            ?>
+          </nav><!-- #site-navigation -->
+        </div>
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
