2bd7bf3746fcb1da7560887e0ef101a3b07fe769
nikolaswise <nikolaswise@gmail.com>
2018-01-04 18:24:22 -0800

    /route

--- themes/lone-goose-press/inc/routing.php
+++ themes/lone-goose-press/inc/routing.php
@@ -11,53 +11,54 @@ Routes::map('/', function($params){
   Routes::load('routes/index.php');
 });
 
-Routes::map('impressions', function($params){
+Routes::map('/impressions', function($params){
   Routes::load('routes/impressions.php');
 });
-Routes::map('impressions/:slug', function($params){
+Routes::map('/impressions/:slug', function($params){
   Routes::load('routes/post.php', $params);
 });
 
-Routes::map('about', function($params){
+Routes::map('/about', function($params){
   Routes::load('routes/about.php');
 });
 
-Routes::map('contact', function($params){
+Routes::map('/contact', function($params){
   Routes::load('routes/contact.php');
 });
 
-Routes::map('checkout', function($params){
+Routes::map('/checkout', function($params){
   Routes::load('routes/checkout.php');
 });
-Routes::map('checkout/confirm', function($params){
+Routes::map('/checkout/confirm', function($params){
   Routes::load('routes/confirm.php');
 });
-Routes::map('checkout/thanks', function($params){
+Routes::map('/checkout/thanks', function($params){
   Routes::load('routes/confirm.php');
 });
-Routes::map('checkout/problem', function($params){
+Routes::map('/checkout/problem', function($params){
   Routes::load('routes/problem.php');
 });
 
-Routes::map('publications', function($params){
+Routes::map('/publications', function($params){
   Routes::load('routes/section.php');
 });
-Routes::map('publications/:category', function($params){
-  Routes::load('routes/section.php');
-});
-
-
-Routes::map('commisions/', function($params){
-  Routes::load('routes/section.php');
-});
-Routes::map('commisions/:category', function($params){
+Routes::map('/publications/:category', function($params){
   Routes::load('routes/section.php');
 });
 
-Routes::map('404', function($params){
+
+Routes::map('/commisions', function($params){
+  Routes::load('routes/section.php');
+});
+
+Routes::map('/commisions/:category', function($params){
+  Routes::load('routes/section.php');
+});
+
+Routes::map('/404', function($params){
   Routes::load('routes/404.php');
 });
 
-Routes::map(':project', function($params){
+Routes::map('/:project', function($params){
   Routes::load('routes/project.php');
 });
